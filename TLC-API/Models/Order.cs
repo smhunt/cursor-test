78585bc70cb82dc49dd8552f0b4c24b856c82d1a
smhunt <sean@ecoworks.ca>
2024-04-05 15:45:43 -0400

    first commit

---
+++ TLC-API/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TLC_API.Models;
+
+public partial class Order
+{
+    public int OrderId { get; set; }
+
+    public int UserId { get; set; }
+
+    public decimal TotalPrice { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public string? StripePaymentId { get; set; }
+
+    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ TLC-API/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TLC_API.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? LastLogin { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    public virtual ICollection<PaymentInfo> PaymentInfos { get; set; } = new List<PaymentInfo>();
+
+    public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+}
